c915ffe5240cf33497ea9a4ae968130312c1d1a4
Valerio <valeriofavarato@gmail.com>
2019-06-18 00:04:19 +0200

    Modified tmux config, added scripts

---
+++ setup-windows.bat
@@ -0,0 +1,5 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco upgrade chocolatey
+
+choco install -y googlechrome jre8 7zip notepadplusplus vlc vcredist140 vscode steam kicad
